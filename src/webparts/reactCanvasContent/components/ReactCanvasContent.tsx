2d854584af33deff73180ccc7cc7acbccf074c7d
Jason Clark <jason_mc@btinternet.com>
2024-08-12 12:30:56 +0100

    gets canvascontent data from dev tenant

--- src/webparts/reactCanvasContent/components/ReactCanvasContent.tsx
+++ src/webparts/reactCanvasContent/components/ReactCanvasContent.tsx
@@ -19,7 +19,12 @@ const ReactCanvasContent: React.FC<IReactCanvasContentProps> = (props) => {
   const [counter, setCounter] = useState<number>(0);
   const [evenOdd, setEvenOdd] = useState<string>('');  
   //const [siteLists, setSiteLists] = useState<string[]>([]);
-  //const [webPartTitle, setWebPartTitle] = useState('');
+  const [webPartTitle, setWebPartTitle] = useState('');
+  const [grouptitle1, setGroupTitle1] = useState('');
+  const [grouptitle2, setGroupTitle2] = useState('');
+  const [grouptitle3, setGroupTitle3] = useState('');
+  const [grouptitle4, setGroupTitle4] = useState('');
+  const [grouptitle5, setGroupTitle5] = useState('');
   
   //componentDidMount
   /*
@@ -44,12 +49,34 @@ const ReactCanvasContent: React.FC<IReactCanvasContentProps> = (props) => {
       const endpoint = `${siteUrl}/_api/sitepages/pages(1)?$select=CanvasContent1&expand=CanvasContent1`;
       const rawResponse = await spHttpClient.get(endpoint, SPHttpClient.configurations.v1);
       const jsonResponse = await rawResponse.json();
-      const canvasContent = jsonResponse.value[0].CanvasContent1;
-      const webPartData = JSON.parse(canvasContent);
-      const title = webPartData.Title;
-      console.log("response json",jsonResponse,title);
+      const jsonCanvasContent = jsonResponse.CanvasContent1;
+      const parseCanvasContent = JSON.parse(jsonCanvasContent);
+      //const webpartData = canvasContent.webPartData;
+      //console.log("canvascontent json",jsonCanvasContent);
+      console.log("canvascontent parse",parseCanvasContent);
+      //parseCanvasContent.forEach((item:any,index:number) => {
+      //let index : number = 0;
+      for(const item of parseCanvasContent){
+        console.log("webPartData Title",item.webPartData.title);
+        let itemTitle : string = item.webPartData.title;
+        let itemGroup1 : string = item.webPartData.properties.Group1Title;
+        let itemGroup2 : string = item.webPartData.properties.Group2Title;
+        let itemGroup3 : string = item.webPartData.properties.Group3Title;
+        let itemGroup4 : string = item.webPartData.properties.Group4Title;
+        let itemGroup5 : string = item.webPartData.properties.Group5Title;
 
-      //setWebPartTitle(title);
+        if(item.webPartData.title === "Important Links"){
+          setWebPartTitle(itemTitle);
+          setGroupTitle1(itemGroup1);
+          setGroupTitle2(itemGroup2);
+          setGroupTitle3(itemGroup3);
+          setGroupTitle4(itemGroup4);
+          setGroupTitle5(itemGroup5);
+          //break;          
+        }
+        //index++;
+      }
+      //})
     })();
   },[siteUrl,spHttpClient]);
 
@@ -80,6 +107,14 @@ const ReactCanvasContent: React.FC<IReactCanvasContentProps> = (props) => {
         <button onClick={() => onButtonClick()}>+</button>
         <div>siteURL : {escape(siteUrl)}</div>
         <div>Web part property value: <strong>{escape(description)}</strong></div>
+      </div>
+      <div>
+        <h1>{webPartTitle}</h1>
+        <h1>{grouptitle1}</h1>
+        <h1>{grouptitle2}</h1>
+        <h1>{grouptitle3}</h1>
+        <h1>{grouptitle4}</h1>
+        <h1>{grouptitle5}</h1>
       </div>        
     </section>
   );
